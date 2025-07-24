a576d1c444e552b2fdaef2115c470f7975d2e63b
DjordjeVujacic4 <djordjevujacic4@gmail.com>
2025-07-24 20:10:30 +0200

    laravel breeze api init

---
+++ routes/api.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\InquiryController;
+use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\PropertyTypeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/property-types', [PropertyTypeController::class, 'index']);
+Route::get('/property-types/{id}', [PropertyTypeController::class, 'show']);
+
+Route::get('/properties', [PropertyController::class, 'index']);
+Route::get('/properties/{id}', [PropertyController::class, 'show']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('property-types', PropertyTypeController::class)
+        ->only(['store', 'update', 'destroy']);
+    Route::resource('properties', PropertyController::class)
+        ->only(['store', 'update', 'destroy']);
+    Route::resource('inquiries', InquiryController::class)
+        ->except(['create', 'edit', 'update']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
