5e784a4349441377c60e2278c6c164911d09f483
DjordjeVujacic4 <djordjevujacic4@gmail.com>
2025-07-26 20:12:56 +0200

    seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+
+        User::factory(5)->create([
+            'role' => 'user',
+        ]);
+
+        $this->call(PropertyTypeSeeder::class);
+        $this->call(PropertySeeder::class);
+        $this->call(InquirySeeder::class);
+    }
+}
